f0b02f823a6d80512678369aaa4440007c85cead
Kausar Khan <kausarrkhan83@gmail.com>
2025-04-21 00:59:00 +0530

    fix again url

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://the-digital-dinner.onrender.com/",
+      "https://the-digital-dinner.onrender.com",
       "http://localhost:5173",
     ],
     credentials: true,
