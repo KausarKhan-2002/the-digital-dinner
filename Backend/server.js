e6a4487eea40988bb22858f2bd0f7cdb1450d6ca
Kausar Khan <kausarrkhan83@gmail.com>
2025-04-21 00:50:19 +0530

    fix url to bypass api's

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://the-digital-client-server.onrender.com",
+      "https://the-digital-client.onrender.com",
       "http://localhost:5173",
     ],
     credentials: true,
