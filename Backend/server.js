864a2b0c750b446125981073ac3e5cbc70208ecb
Kausar Khan <kausarrkhan83@gmail.com>
2025-04-24 16:26:17 +0530

    url by pass

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://the-digital-dinner.onrender.com",
+      "https://the-digital-dinner.onrender.com/",
       "http://localhost:5173",
     ],
     credentials: true,
